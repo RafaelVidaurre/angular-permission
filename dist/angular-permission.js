cb6f27b9b58434bc63d1b20a0f0a88094b755031
Rafael Vidaurre <narzerus@gmail.com>
2014-10-31 16:48:31 -0300

    Bump to correct version (conflict with branching from outdated commit)

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -1,7 +1,7 @@
 /**
  * angular-permission
  * Route permission and access control as simple as it can get
- * @version v0.1.1 - 2014-10-31
+ * @version v0.1.2 - 2014-10-31
  * @link http://www.rafaelvidaurre.com
  * @author Rafael Vidaurre <narzerus@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
