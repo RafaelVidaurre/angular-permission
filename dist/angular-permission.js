a60e08de5aef13ef0088b2e2b1c350c84f13c842
Rafael Vidaurre <narzerus@gmail.com>
2014-11-07 15:11:46 -0300

    Bump version

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -1,7 +1,7 @@
 /**
  * angular-permission
  * Route permission and access control as simple as it can get
- * @version v0.1.3 - 2014-11-04
+ * @version v0.1.4 - 2014-11-07
  * @link http://www.rafaelvidaurre.com
  * @author Rafael Vidaurre <narzerus@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -33,26 +33,31 @@
           event.preventDefault();
 
           Permission.authorize(permissions, toParams).then(function () {
-            $rootScope.$broadcast("$stateChangePermissionAccepted");
 
             // If authorized, use call state.go without triggering the event.
             // Then trigger $stateChangeSuccess manually to resume the rest of the process
             // Note: This is a pseudo-hacky fix which should be fixed in future ui-router versions
-            $state.go(toState.name, toParams, {notify: false}).then(function() {
-              $rootScope
-                .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
-            });
+            if (!$rootScope.$broadcast('$stateChangeStart', toState.name, toParams, fromState.name, fromParams).defaultPrevented) {
+              $rootScope.$broadcast('$stateChangePermissionAccepted');
 
-          }, function () {
-            $rootScope.$broadcast("$stateChangePermissionDenied");
-
-            // If not authorized, redirect to wherever the route has defined, if defined at all
-            var redirectTo = permissions.redirectTo;
-            if (redirectTo) {
-              $state.go(redirectTo, {}, {notify: false}).then(function() {
+              $state.go(toState.name, toParams, {notify: false}).then(function() {
                 $rootScope
                   .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
               });
+            }
+
+          }, function () {
+            if (!$rootScope.$broadcast('$stateChangeStart', toState.name, toParams, fromState.name, fromParams).defaultPrevented) {
+              $rootScope.$broadcast('$stateChangePermissionDenied');
+
+              // If not authorized, redirect to wherever the route has defined, if defined at all
+              var redirectTo = permissions.redirectTo;
+              if (redirectTo) {
+                $state.go(redirectTo, {}, {notify: false}).then(function() {
+                  $rootScope
+                    .$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams);
+                });
+              }
             }
           });
         }
--- src/permission.mdl.js
+++ src/permission.mdl.js
@@ -24,10 +24,10 @@
           event.preventDefault();
 
           Permission.authorize(permissions, toParams).then(function () {
+
             // If authorized, use call state.go without triggering the event.
             // Then trigger $stateChangeSuccess manually to resume the rest of the process
             // Note: This is a pseudo-hacky fix which should be fixed in future ui-router versions
-            
             if (!$rootScope.$broadcast('$stateChangeStart', toState.name, toParams, fromState.name, fromParams).defaultPrevented) {
               $rootScope.$broadcast('$stateChangePermissionAccepted');
 
