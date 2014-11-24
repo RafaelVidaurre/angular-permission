afa8f9331dad0c4eca28cb30f5fdc4f242503e01
Rafael Vidaurre <narzerus@gmail.com>
2014-11-24 15:54:37 -0300

    Bump version

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -1,7 +1,7 @@
 /**
  * angular-permission
  * Route permission and access control as simple as it can get
- * @version v0.1.5 - 2014-11-23
+ * @version v0.1.6 - 2014-11-24
  * @link http://www.rafaelvidaurre.com
  * @author Rafael Vidaurre <narzerus@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
