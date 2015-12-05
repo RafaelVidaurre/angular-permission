0ffa8063880e60cdedd5037d98781900eb74a444
Błażej Krysiak <blazej.krysiak@gmail.com>
2015-12-05 01:53:27 +0100

    chore: smallfix

--- config/karma/karma.conf.js
+++ config/karma/karma.conf.js
@@ -51,7 +51,6 @@ module.exports = function(config) {
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-chrome-launcher',
       'karma-mocha-reporter',
       'karma-jasmine'
     ],
