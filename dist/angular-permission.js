0d5ee4252fe5855c7f99fe05947097f30930ed3c
Błażej Krysiak <blazej.krysiak@gmail.com>
2016-01-29 20:06:58 +0100

    Bump version to 2.0.1

--- dist/angular-permission.js
+++ dist/angular-permission.js
@@ -1,7 +1,7 @@
 /**
  * angular-permission
  * Route permission and access control as simple as it can get
- * @version v2.0.0 - 2016-01-29
+ * @version v2.0.1 - 2016-01-29
  * @link http://www.rafaelvidaurre.com
  * @author Rafael Vidaurre <narzerus@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
