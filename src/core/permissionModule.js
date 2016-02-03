3c2f0554e7ab090b1d9cce4f0fd70b22b1672853
Błażej Krysiak <blazej.krysiak@gmail.com>
2016-02-03 09:07:54 +0100

    #81 Removed triggering $stateChangeSuccess. Solving issue with order of browser history and problem with ui-router-extras.
    Either I am super smart or super dumb. But it works.

--- src/core/permissionModule.js
+++ src/core/permissionModule.js
@@ -28,7 +28,6 @@
         event.preventDefault();
         setStateAuthorizationStatus(true);
 
-
         if (!areStateEventsDefaultPrevented()) {
           var compensatedPermissionMap = compensatePermissionMap(toState.data.permissions);
           authorizeForState(compensatedPermissionMap);
@@ -90,33 +89,18 @@
       /**
        * Handles state authorization
        *
-       * @param permissions {Object} Map of "only" or "except" permission names
+       * @param permissions {PermissionMap} Map of permission names
        */
       function authorizeForState(permissions) {
         Authorization
           .authorize(permissions, toParams)
           .then(function () {
             $rootScope.$broadcast('$stateChangePermissionAccepted', toState, toParams, options);
-            goToState(toState.name);
+            $state.go(toState.name, toParams, options);
           })
           .catch(function (rejectedPermission) {
             $rootScope.$broadcast('$stateChangePermissionDenied', toState, toParams, options);
             permissions.redirectToState(rejectedPermission);
-          });
-      }
-
-      /**
-       * Redirects to states when permissions are met
-       *
-       * If authorized, use call state.go without triggering the event.
-       * Then trigger $stateChangeSuccess manually to resume the rest of the process
-       * Note: This is a pseudo-hacky fix which should be fixed in future ui-router versions
-       */
-      function goToState(name) {
-        $state
-          .go(name, toParams, angular.extend({}, options, {notify: false}))
-          .then(function () {
-            $rootScope.$broadcast('$stateChangeSuccess', toState, toParams, fromState, fromParams, options);
           });
       }
 
