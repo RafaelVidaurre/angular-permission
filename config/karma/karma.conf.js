c8871a53d323a650bf8f44ee8ad55799098572c6
Błażej Krysiak <blazej.krysiak@gmail.com>
2016-08-18 20:29:08 +0200

    chore: Changed karma log level to debug.

--- config/karma/karma.conf.js
+++ config/karma/karma.conf.js
@@ -72,13 +72,13 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false,
+    singleRun: true,
 
     colors: true,
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO
+    logLevel: config.LOG_DEBUG
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
