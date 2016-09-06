0598a88163ddcf44d40999451d546819a8744d0e
Błażej Krysiak <blazej.krysiak@gmail.com>
2016-09-06 21:55:04 +0200

    chore: Protractor configuration fixes

--- config/protractor/protractor.conf.js
+++ config/protractor/protractor.conf.js
@@ -13,6 +13,7 @@ exports.config = {
   sauceKey: process.env.SAUCE_ACCESS_KEY,
 
   capabilities: {
+    'browserName': 'chrome',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     'build': process.env.TRAVIS_BUILD_NUMBER
   },
