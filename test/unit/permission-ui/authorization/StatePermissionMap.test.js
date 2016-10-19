d947c332adbc76cb5df4b9fd955d08f9e81d2651
Joel Jeske <jjeske@netspend.com>
2016-10-19 17:00:57 -0500

    Ensure parent permissions are evaluated only once

--- test/unit/permission-ui/authorization/StatePermissionMap.test.js
+++ test/unit/permission-ui/authorization/StatePermissionMap.test.js
@@ -21,10 +21,14 @@ describe('permission.ui', function () {
           // GIVEN
           var state = jasmine.createSpyObj('state', ['$$permissionState']);
           state.$$permissionState.and.callFake(function () {
+            var parent = {permissions: {only: ['accepted'], except: ['denied']}};
+            var child = Object.create(parent);
+            child.permissions = {only: ['acceptedChild'], except: ['deniedChild']};
+
             return {
               path: [
-                {data: {permissions: {only: ['accepted'], except: ['denied']}}},
-                {data: {permissions: {only: ['acceptedChild'], except: ['deniedChild']}}}
+                {data: child},
+                {data: parent}
               ]
             };
           });
@@ -33,8 +37,38 @@ describe('permission.ui', function () {
           var map = new PermStatePermissionMap(state);
 
           // THEN
-          expect(map.only).toEqual([['accepted'], ['acceptedChild']]);
-          expect(map.except).toEqual([['denied'], ['deniedChild']]);
+          expect(map.only).toEqual([['acceptedChild'], ['accepted']]);
+          expect(map.except).toEqual([['deniedChild'], ['denied']]);
+          expect(map.redirectTo).not.toBeDefined();
+        });
+
+        it('should not duplicate parent state inheritance if childs dont have permissions', function () {
+          // GIVEN
+          var state = jasmine.createSpyObj('state', ['$$permissionState']);
+          state.$$permissionState.and.callFake(function () {
+            var grandparent = {permissions: {only: ['accepted'], except: ['denied']}};
+            var parent = Object.create(grandparent);
+            parent.permissions = {
+              only: ['acceptedChild'],
+              except: ['deniedChild']
+            };
+            var child = Object.create(parent);
+
+            return {
+              path: [
+                {data: child},
+                {data: parent},
+                {data: grandparent}
+              ]
+            };
+          });
+
+          // WHEN
+          var map = new PermStatePermissionMap(state);
+
+          // THEN
+          expect(map.only).toEqual([['acceptedChild'], ['accepted']]);
+          expect(map.except).toEqual([['deniedChild'], ['denied']]);
           expect(map.redirectTo).not.toBeDefined();
         });
       });
