5b20b1fd53e47e93f219697f5712291227563e77
Błażej Krysiak <blazej.krysiak@gmail.com>
2017-01-31 21:43:28 +0100

    chore: Bump version to 5.2.1

--- dist/angular-permission-ng.js
+++ dist/angular-permission-ng.js
@@ -1,7 +1,7 @@
 /**
  * angular-permission-ng
  * Extension module of angular-permission for access control within angular-route
- * @version v5.1.0 - 2017-01-30
+ * @version v5.1.1 - 2017-01-31
  * @link https://github.com/Narzerus/angular-permission
  * @author Rafael Vidaurre <narzerus@gmail.com> (http://www.rafaelvidaurre.com), Blazej Krysiak <blazej.krysiak@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
